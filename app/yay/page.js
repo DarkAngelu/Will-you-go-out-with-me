5dd1ee1a381c66099dba9410fb0fa3bab8d4d841
DarkAngelu <iamdarkangelu@gmail.com>
2025-01-04 16:03:55 +0530

    Changed in Anirban (Impossible)

--- app/yay/page.js
+++ app/yay/page.js
@@ -1,10 +1,8 @@
 export default function page() {
-
-  return (
-    <div className='box'>
-      <img alt='yayyy' className='gif' src='/gumball-and-darwing.gif' />
-      {'Yayyy~ lets plan a date <3'}
-    </div>
-  )
-
+	return (
+		<div className="box">
+			<img alt="yayyy" className="gif" src="/gumball-and-darwing.gif" />
+			{"Yayyy~ lets plan a date <3"}
+		</div>
+	);
 }
